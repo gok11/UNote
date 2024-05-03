84140f2299071201abb9568cfa290bf22b5bc45b
gok11 <backlight1144@gmail.com>
2024-05-03 21:50:54 +0900

    add UNote editor pane

---
+++ Assets/UNote/Editor/Scripts/GUI/UNoteEditor/UNoteEditorCenterPane.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace UNote.Editor
+{
+    public class UNoteEditorCenterPane : VisualElement
+    {
+        public UNoteEditorCenterPane()
+        {
+            name = nameof(UNoteEditorCenterPane);
+        }
+
+        public void FilterNotesBySearchText() { }
+    }
+}
