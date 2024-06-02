9401346ca310cf426e7e8275f7ad7548126b9c89
gok11 <backlight1144@gmail.com>
2024-06-02 21:39:35 +0900

    左ペインでカテゴリを選択できるように

--- Assets/UNote/Editor/Scripts/FilePaths.cs
+++ Assets/UNote/Editor/Scripts/FilePaths.cs
@@ -4,9 +4,9 @@ namespace UNote.Editor
 {
     public static class UxmlPath
     {
-        public static readonly string NoteCategoryItem = AssetDatabase.GUIDToAssetPath("46f56de1c003d7b43b893a3c3f6be94f");
         public static readonly string NoteContent = AssetDatabase.GUIDToAssetPath("966d495779e1f3341bdb1fd5027ef2e7");
         public static readonly string NoteListItem = AssetDatabase.GUIDToAssetPath("89d831667923bd542bf015fb582fd32e");
+        public static readonly string NoteTypeItem = AssetDatabase.GUIDToAssetPath("46f56de1c003d7b43b893a3c3f6be94f");
         public static readonly string UNoteEditorCenterPane = AssetDatabase.GUIDToAssetPath("5a2bb31080c69ad42811c6de0ec2c8b2");
         public static readonly string UNoteEditorLeftPane = AssetDatabase.GUIDToAssetPath("2b2d08b882ec10f4fa1d91efcdc74365");
         public static readonly string UNoteEditorRightPane = AssetDatabase.GUIDToAssetPath("a23f05ef4e72fe445825f193e8fadc2b");
