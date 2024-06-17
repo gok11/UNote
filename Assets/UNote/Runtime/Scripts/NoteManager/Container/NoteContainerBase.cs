d3db2a3fe7cf22e39d980786aba7a84b3ff0745f
gok11 <backlight1144@gmail.com>
2024-06-18 00:28:14 +0900

    メモシリアライズ周りをある程度基底クラスに逃がした

--- Assets/UNote/Runtime/Scripts/NoteManager/Container/NoteContainerBase.cs
+++ Assets/UNote/Runtime/Scripts/NoteManager/Container/NoteContainerBase.cs
@@ -7,6 +7,8 @@ namespace UNote.Runtime
 {
     public abstract class NoteContainerBase : ScriptableObject
     {
+        #region Property
+        
         protected abstract string Identifier { get; }
 
         protected abstract string SubDirectoryName { get; }
@@ -29,5 +31,58 @@ namespace UNote.Runtime
         {
             get { return Path.Combine(FileDirectory, OwnFileName); }
         }
+        
+        #endregion // Property
+
+        public abstract void Load();
+        
+        protected void Load<T>() where T : class
+        {
+            string authorName = UserConfig.GetUNoteSetting().UserName;
+
+            if (File.Exists(OwnFileFullPath))
+            {
+                string json = File.ReadAllText(OwnFileFullPath);
+                var container = JsonUtility.FromJson<T>(json);
+                LoadData(container, authorName);
+            }
+            else
+            {
+                Save();
+            }
+
+            string[] filePaths = Directory.GetFiles(FileDirectory, "*.json");
+            foreach (var projectNotePath in filePaths)
+            {
+                if (projectNotePath == OwnFileFullPath)
+                {
+                    continue;
+                }
+
+                string otherAuthorName = Path.GetFileNameWithoutExtension(projectNotePath);
+                string otherJson = File.ReadAllText(projectNotePath);
+                var container = JsonUtility.FromJson<T>(otherJson);
+                LoadData(container, otherAuthorName);
+            }
+        }
+        
+        protected abstract void LoadData<T>(T container, string authorName) where T : class;
+
+        public abstract void Save();
+        
+        protected void Save<T>(T container) where T : class
+        {
+            if (!Directory.Exists(FileDirectory))
+            {
+                Directory.CreateDirectory(FileDirectory);
+            }
+            
+            string json = JsonUtility.ToJson(container);
+            File.WriteAllText(OwnFileFullPath, json);
+
+            ClearCache();
+        }
+
+        public abstract void ClearCache();
     }
 }
