8fd66c698022459020c5df10bc63688bd062d752
gok11 <backlight1144@gmail.com>
2024-06-24 22:21:57 +0900

    アセットメモの追加対応

---
+++ Assets/UNote/Editor/Scripts/GUI/UNoteEditor/AssetNoteAddWindow.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace UNote.Editor
+{
+    public class AssetNoteAddWindow : EditorWindow
+    {
+        private void CreateGUI()
+        {
+            // Label
+            Label label = new Label("Bind Asset");
+            
+            // Bind Asset
+            ObjectField assetField = new ObjectField("Target Asset");
+            assetField.allowSceneObjects = false;
+
+            // Button
+            Button addButton = new Button();
+            addButton.text = "Add Asset Note";
+            addButton.SetEnabled(false);
+            addButton.clicked += () =>
+            {
+                AssetNote assetNote = EditorUNoteManager.AddOrGetAssetNote(assetField.value);
+                EditorUNoteManager.SelectRoot(assetNote);
+                
+                Close();
+            };
+
+            assetField.RegisterValueChangedCallback(t =>
+            {
+                addButton.SetEnabled(t.newValue);
+            });
+            
+            rootVisualElement.Add(label);
+            rootVisualElement.Add(assetField);
+            rootVisualElement.Add(addButton);
+        }
+    }
+}
